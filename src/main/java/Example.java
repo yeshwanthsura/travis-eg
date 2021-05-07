9f751ed6f411e8e345aea5212b7219498714cad6
Sai-Yeshwanth-Reddy-Sura <sai.yeshwanth.reddy.sura@in.ibm.com>
2021-05-07 11:46:26 +0530

    commit2 to b2

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -8,4 +8,8 @@ public class Example {
     public static void addTwo() {
         System.out.println("added two");
     }
+    public static void addThree() {
+        System.out.println("added three");
+    }
+
 }
